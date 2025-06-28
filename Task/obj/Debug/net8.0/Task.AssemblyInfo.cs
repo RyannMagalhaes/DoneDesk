08da658efc85e63a1173c3105dcff3c7527726ba
Ryann Cláudio <ryanncmm2002@gmail.com>
2025-06-28 01:09:28 -0300

    [BUILD] database files update

--- Task/obj/Debug/net8.0/Task.AssemblyInfo.cs
+++ Task/obj/Debug/net8.0/Task.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf45118979dec662d1e76f4c20a794fff1f5764c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d57e580540f64e755817eb379ce4eafecfc4218")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
