5fbbf6e877a93797dad4507f4e114b8371c0dd74
Ryann Cláudio <ryanncmm2002@gmail.com>
2025-06-28 14:54:43 -0300

    [FEAT] implementa estilização do componente task-card

--- done-desk/src/tasks/task-card/task-card.component.ts
+++ done-desk/src/tasks/task-card/task-card.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { Calendar, Clock, MoreVertical, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-task-card',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.css'
 })
 export class TaskCardComponent {
-
+  readonly Calendar = Calendar;
+  readonly Clock = Clock;
+  readonly MoreVertical = MoreVertical;
 }
--- done-desk/src/tasks/view-tasks/view-tasks.component.ts
+++ done-desk/src/tasks/view-tasks/view-tasks.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, Plus } from 'lucide-angular';
+import { TaskCardComponent } from "../task-card/task-card.component";
 
 @Component({
   selector: 'app-view-tasks',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule, TaskCardComponent],
   templateUrl: './view-tasks.component.html',
   styleUrl: './view-tasks.component.css'
 })
 export class ViewTasksComponent {
-
+  readonly Plus = Plus;
 }
