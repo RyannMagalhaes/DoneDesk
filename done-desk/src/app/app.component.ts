a546656ab876efe34c5edb2cdc1833af9f7a00fa
Ryann Cláudio <ryanncmm2002@gmail.com>
2025-06-28 13:31:48 -0300

    [BUILD] instala a biblioteca primeflex

--- done-desk/src/app/app.component.ts
+++ done-desk/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LucideAngularModule } from 'lucide-angular';
+import { Check, Clock, ListCheck, LucideAngularModule, Plus } from 'lucide-angular';
+import { ViewTasksComponent } from "../tasks/view-tasks/view-tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LucideAngularModule],
+  imports: [RouterOutlet, LucideAngularModule, ViewTasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'done-desk';
+
+  readonly ListCheck = ListCheck;
 }
--- done-desk/src/tasks/tasks.module.ts
+++ done-desk/src/tasks/tasks.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { Check, Clock, ListCheck, LucideAngularModule, Plus } from 'lucide-angular';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, LucideAngularModule.pick({ Plus, Check, Clock, ListCheck })],
 })
-export class TasksModule { }
+export class TasksModule {}
